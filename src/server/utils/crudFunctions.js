31162e79811112c55762d1ecd393b293a8e3fafc
lesteven <le_steven@outlook.com>
2018-04-17 09:41:41 -0700

    add comments to crudFunctions

--- src/server/utils/crudFunctions.js
+++ src/server/utils/crudFunctions.js
@@ -1,14 +1,27 @@
 var mongoose = require('mongoose');
 
 
+/*
+* Delete function, pass in callback, 
+* model, and parameters to delete from db.
+* Callback will send response back to client 
+*/
+
 exports.delete = (req,res,model,cb) =>{
 	model.findOneAndRemove(req.body).exec(function(err,removed){
 		if(err){
 			//console.log(err);
+			res.json({err:'error'})
 		}
 		cb(req,res,model);
 	})
 }
+
+/*
+* Post function, pass in callback, 
+* model, and parameters to post to db.
+* Callback will send response back to client 
+*/
 
 exports.post = (req,res,model) =>{
 	var content = new model(req.body);
@@ -21,9 +34,14 @@ exports.post = (req,res,model) =>{
 	})
 }
 
+/*
+* Put function, pass in callback, 
+* model, and parameters to update  db.
+* Callback will send response back to client 
+* id will map to correct item on client 
+*/
+
 exports.put = (res,model,id,change) =>{
-    //console.log('put function');
-    //console.log(id,change);
     
     model.findOneAndUpdate(id,change).exec(function(err,update){
         if(err){
@@ -39,3 +57,5 @@ exports.put = (res,model,id,change) =>{
         })
     })
 }
+
+
