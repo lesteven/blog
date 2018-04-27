41483693c4ce9584f28e3c6c8848527a1891ecbb
Steven <le_steven@outlook.com>
2018-04-27 15:02:35 -0700

    add image preview

--- src/common/adminViews/upload/DropImages.jsx
+++ src/common/adminViews/upload/DropImages.jsx
@@ -6,14 +6,21 @@ import { dropAct } from '../../reduxModules/uploadModule';
 
 class DropImages extends Component {
   render() {
-  const { dropAct } = this.props;
+  const { dropAct, upload } = this.props;
     return (
-      <div className='drop-images'>
-        <DropZone className='dropzone'
-             onDrop = { dropAct.bind(this) }>
-          <p> Drag and drop images or click! </p>
-        </DropZone>
-      </div>
+      <Fragment>
+        <div className='drop-images'>
+          <DropZone className='dropzone'
+               onDrop = { dropAct.bind(this) }>
+            <p> Drag and drop images or click! </p>
+          </DropZone>
+        </div>
+        <div className='uploaded-images'>
+          <h2> Uploaded Images </h2>
+          { upload.files.map(f => 
+            <img key = {f.name} src= {f.preview} />) }
+        </div>
+      </Fragment>
     )
   }
 }
