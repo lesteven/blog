331141ecde4b6f1b528788a79895a9b8286e5a90
Steven <le_steven@outlook.com>
2018-05-22 15:34:32 -0700

    update EditBlog selector

--- src/common/adminViews/blog2/containers/EditBlog.jsx
+++ src/common/adminViews/blog2/containers/EditBlog.jsx
@@ -2,11 +2,16 @@ import React, { Component, Fragment } from 'react';
 import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
 
-
+/*
+* EditBlog retrieves own data from reselect
+* so once it is edited, it will only render itself
+* and not other components.
+*/
 class EditBlog extends Component {
   
   render() {
   console.log(this.props);
+//  console.log(this.props.data);
     return (
       <div>hello</div>
     )
@@ -14,11 +19,16 @@ class EditBlog extends Component {
 }
 
 const getData = createSelector(
-
+  (data, props) => props.id,
+  (data) => data,
+  (id, data) => data
+    .filter(blog => blog._id === id)[0]
 )
 
-const mapState  = (state, ownProps) => ({
-  data: getData(state, ownProps) 
+const mapState  = ({ richEditor }, ownProps) => ({
+  data: getData(richEditor.converted, ownProps) 
 })
 
-export default EditBlog;
+//export default EditBlog;
+export default connect(mapState)(EditBlog);
+
