69915eda0520cdaf91c4ea5eb02c219412f10e98
Steven <le_steven@outlook.com>
2018-05-24 13:49:39 -0700

    optimize paginate render

--- src/common/adminViews/blog2/containers/EditPaginate.jsx
+++ src/common/adminViews/blog2/containers/EditPaginate.jsx
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import Paginate from '../../../sharedViews/paginate/Paginate.jsx';
 import { withRouter } from 'react-router-dom';
 import { getBlog } from './EditBlogList';
+import { createSelector } from 'reselect';
 
 
 class EditPaginate extends Component {
 	blogID=()=>{
-    const { ids } = this.props.richEditor
-    // const { ids } = this.props;
+//    const { ids } = this.props.richEditor
+    const { ids } = this.props;
     if(ids[0]){
         let obj ={};
         obj.new = ids[0];
@@ -21,8 +22,9 @@ class EditPaginate extends Component {
     }
 	}
   render() {
-//    console.log(this.props);
-    const { pagination } = this.props.richEditor;
+    // console.log('editpaginate', this.props);
+    // const { pagination } = this.props.richEditor;
+    const { pagination } = this.props;
     const { path } = this.props.match;
     return (
       <Fragment>
@@ -38,10 +40,15 @@ class EditPaginate extends Component {
   }
 }
 
+const getPage = createSelector(
+  (richEditor) => richEditor.pagination,
+  (pagination) => (pagination)
+);
 
 const mapState = ({ richEditor }) => ({
-//  ids: getBlog(richEditor),
-  richEditor,
+  ids: getBlog(richEditor),
+  pagination: getPage(richEditor),
+//  richEditor,
 })
 
 
