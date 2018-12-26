fce2e76e6bb5764e9fa2ddd2b4170be5331aa206
lesteven <le_steven@outlook.com>
2018-12-26 13:49:27 -0800

    update about page

--- src/common/views/about/About.jsx
+++ src/common/views/about/About.jsx
@@ -1,17 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import styles from './about.css';
-
+import data from './data';
 
 class About extends Component {
     
     render (){
-        return (
-            <Fragment>
-            <div className='about'>
-                hello about!
-            </div>
-            </Fragment>
-        )
+      return (
+        <div className='about'>
+          { data.map((each, index) => 
+            <p key = { index }> { each } </p> ) }
+          <p> You can check out the code at 
+            <a href= 'https://github.com/lesteven/blog'> github</a> 
+            . Or visit my website at 
+            <a href = 'https://imstevenle.com'> imstevenle.com</a>
+            .
+          </p>
+        </div>
+      )
     }
 }
 
---
+++ src/common/views/about/data.js
@@ -0,0 +1,7 @@
+const data = [
+  "Hi, this is a blog that I created from scratch (including the use of libraries of course). ",
+  "For the front-end, it was built using react, redux and react-router. Draft.js was used for the WYSIWYG editor. I made customization to the editor to include images and youtube videos in the posts.",
+  "For the back-end, I used node and mongodb. The blog pagination was custom made.",
+]
+
+export default data;
